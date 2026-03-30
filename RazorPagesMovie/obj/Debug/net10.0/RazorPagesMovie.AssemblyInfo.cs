9f821fdec2ef8b6d244554e8926bf85561af590e
westb <westboudreaux@gmail.com>
2026-03-30 12:56:28 -0500

    more stuff

--- RazorPagesMovie/obj/Debug/net10.0/RazorPagesMovie.AssemblyInfo.cs
+++ RazorPagesMovie/obj/Debug/net10.0/RazorPagesMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a95c6209a3a24ccdd55c55800d1fb7897dd828b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0b6bb080a412694b0ca08d1b05eaee9ee3d2fd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
