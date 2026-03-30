3250a76cd0df80d5c6f56fa4ce6b4a8156f94a96
SamCT <colliertenisonsa@hendrix.edu>
2026-03-30 12:55:35 -0500

    Auto stash before merge of "main" and "origin/main"

--- RazorPagesMovie/obj/Debug/net10.0/RazorPagesMovie.AssemblyInfo.cs
+++ RazorPagesMovie/obj/Debug/net10.0/RazorPagesMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+950045abdbe01ab19fbda739fe0213bdfdf825de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a95c6209a3a24ccdd55c55800d1fb7897dd828b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
