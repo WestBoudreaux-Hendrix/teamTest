b14319a3fb5221b11a37bf6bc90730c780f02e86
SamCT <colliertenisonsa@hendrix.edu>
2026-04-01 14:56:46 -0500

    added Gitignore

--- RazorPagesMovie/obj/Debug/net10.0/RazorPagesMovie.AssemblyInfo.cs
+++ RazorPagesMovie/obj/Debug/net10.0/RazorPagesMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b332d2f33d9f7252db85590bb8992ccea471c41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05be8064c004ffa1fb9818e48e6358d05c1ed1ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
